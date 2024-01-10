9abc7e88ffa671f5244584e1798560e5aaaa3b19
Matimrs <mati.morales.tepp@gmail.com>
2024-01-10 18:48:54 -0300

    IMplementacion de estilos con Bulma

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 
 const NavBar = () => {
     return(
-        <nav>
-            <div>
-                <img alt="Logo"/>
+        <nav class="navbar is-flex is-flex-direction-row is-justify-content-center is-align-items-center" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="">
+                <img src="" alt="Logo" width="112" height="28"/>
+                </a>
             </div>
         </nav>
     )
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -5,10 +5,15 @@ const Person = ({id,name,gasto,handlePersonDelete}) => {
         }
 
 return(
-    <div>
-        <p><b>{name}</b> gasto: <b>${gasto}</b></p>
-        <button onClick={personDelete}>x</button>
-    </div>
+    <article class="message is-dark container">
+        <div class="message-header">
+            <p>{name}</p>
+            <button class="delete" aria-label="delete" onClick={personDelete}></button>
+        </div>
+        <div class="message-body">
+            ${gasto}
+        </div>
+    </article>
 )
 }
 
