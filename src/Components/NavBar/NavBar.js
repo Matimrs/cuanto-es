c736c1f783497b781407331d824095e8c5785156
Matimrs <mati.morales.tepp@gmail.com>
2024-01-11 00:59:26 -0300

    Correccion href navbar}

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ const NavBar = () => {
     return(
         <nav class="navbar is-flex is-flex-direction-row is-justify-content-center is-align-items-center" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="">
+                <a class="navbar-item" href="https://github.com/Matimrs">
                 <img src="" alt="Logo" width="112" height="28"/>
                 </a>
             </div>
