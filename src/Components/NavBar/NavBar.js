77dafe7a66814398fa3a415a8e52db05f0003438
Matimrs <mati.morales.tepp@gmail.com>
2024-01-11 01:32:20 -0300

    Cambio de titulo e imagen

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
         <nav class="navbar is-flex is-flex-direction-row is-justify-content-center is-align-items-center" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://github.com/Matimrs">
-                <img src="" alt="Logo" width="112" height="28"/>
+                <img src="Logo.jpeg" alt="Logo"/>
                 </a>
             </div>
         </nav>
