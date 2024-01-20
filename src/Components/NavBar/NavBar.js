fb5de8a64a458b7c90f0699db52142ea093f8d85
Matias Morales <mati.morales.tepp@gmail.com>
2024-01-20 14:20:28 -0300

    Cambio de logo navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
         <nav class="navbar is-flex is-flex-direction-row is-justify-content-center is-align-items-center" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://github.com/Matimrs">
-                <img src="Logo.jpeg" alt="Logo"/>
+                <img src="LogoM.png" alt="Logo"/>
                 </a>
             </div>
         </nav>
